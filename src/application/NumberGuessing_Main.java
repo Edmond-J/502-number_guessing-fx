4d12c473e89791c2db7ac413d4844e7c6d653e56
jam <jam@10.140.150.120>
2023-09-23 00:47:32 +1200

    -removed the lookup method, and put all the element in the @FXML field

--- src/application/NumberGuessing_Main.java
+++ src/application/NumberGuessing_Main.java
@@ -1,9 +1,7 @@
 package application;
 
-import java.io.IOException;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
@@ -12,14 +10,17 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 
 public class NumberGuessing_Main extends Application {
 //	static Pane game, setting;
-	Parent game;
-	Parent setting;
+	private Parent game;
+	private Parent setting;
 	private int min = 0, max = 20, attempts = 5, yourGuess, target;
+	@FXML
+	private TextField startText, rangeText, attamptsText,guessText;
+	@FXML
+	private Button applyButton, settingButton, submitButton;
 
 	public void initialize() {
 		target = (int)(min+Math.random()*(max-min));
@@ -33,18 +34,23 @@ public class NumberGuessing_Main extends Application {
 		initialize();
 		try {
 			Pane container = new Pane();
-			game = FXMLLoader.load(getClass().getResource("game.fxml"));
-			setting = FXMLLoader.load(getClass().getResource("setting.fxml"));
+			FXMLLoader gameLoader= new FXMLLoader(getClass().getResource("game.fxml"));
+			gameLoader.setController(this);
+			game = gameLoader.load();
+//			setting = FXMLLoader.load(getClass().getResource("setting.fxml"));
+			FXMLLoader settingLoader = new FXMLLoader(getClass().getResource("setting.fxml"));
+			settingLoader.setController(this);
+			setting = settingLoader.load();
 			setting.setVisible(false);
 			container.getChildren().addAll(game, setting);
 			System.out.println(game.toString());
-			TextField startText = (TextField)setting.lookup("#startText");
+//			TextField startText = (TextField)setting.lookup("#startText");
 			startText.setPromptText(""+min);
-			TextField rangeText = (TextField)setting.lookup("#rangeText");
+//			TextField rangeText = (TextField)setting.lookup("#rangeText");
 			rangeText.setPromptText(""+(max-min));
-			TextField attamptsText = (TextField)setting.lookup("#attamptsText");
+//			TextField attamptsText = (TextField)setting.lookup("#attamptsText");
 			attamptsText.setPromptText(""+attempts);
-			Button applyButton = (Button)setting.lookup("#applyButton");
+//			Button applyButton = (Button)setting.lookup("#applyButton");
 			applyButton.setOnMouseClicked(e -> {
 				if (startText.getText().length() > 0)
 					min = Integer.valueOf(startText.getText());
@@ -54,16 +60,16 @@ public class NumberGuessing_Main extends Application {
 					attempts = Integer.valueOf(attamptsText.getText());
 				setting.setVisible(false);
 			});
-			Label result = (Label)game.lookup("#result");
-			Button settingButton = (Button)game.lookup("#settingButton");
+				Label result = (Label)game.lookup("#result");
+//			Button settingButton = (Button)game.lookup("#settingButton");
 			settingButton.setOnMouseClicked(e -> {
 				if (setting.isVisible())
 					setting.setVisible(false);
 				else setting.setVisible(true);
 			});
-			Button submitButton = (Button)game.lookup("#submitButton");
+//			Button submitButton = (Button)game.lookup("#submitButton");
 			submitButton.setOnMouseClicked(e -> {
-				TextField guessText = (TextField)game.lookup("#guessText");
+//				TextField guessText = (TextField)game.lookup("#guessText");
 				if (guessText.getText().length() <= 0 || !onlyNumber(guessText.getText())) {
 					result.setText("invalid input!");
 					return;
