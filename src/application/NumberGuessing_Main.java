180067cf19b3380a66e76cf8c191298a347cbc8a
jam <jam@10.140.150.120>
2023-09-22 16:11:55 +1200

    -after win, game will reset, can continue to play
    settingButton can call and remove the setting pane

--- src/application/NumberGuessing_Main.java
+++ src/application/NumberGuessing_Main.java
@@ -21,9 +21,16 @@ public class NumberGuessing_Main extends Application {
 	Parent setting;
 	private int min = 0, max = 20, attempts = 5, yourGuess, target;
 
+	public void initialize() {
+		target = (int)(min+Math.random()*(max-min));
+		min = 0;
+		max = 20;
+		attempts = 5;
+	}
+
 	@Override
 	public void start(Stage primaryStage) {
-		target = (int)(min+Math.random()*(max-min));
+		initialize();
 		try {
 			Pane container = new Pane();
 			game = FXMLLoader.load(getClass().getResource("game.fxml"));
@@ -50,33 +57,42 @@ public class NumberGuessing_Main extends Application {
 			Label result = (Label)game.lookup("#result");
 			Button settingButton = (Button)game.lookup("#settingButton");
 			settingButton.setOnMouseClicked(e -> {
-				setting.setVisible(true);
+				if (setting.isVisible())
+					setting.setVisible(false);
+				else setting.setVisible(true);
 			});
 			Button submitButton = (Button)game.lookup("#submitButton");
 			submitButton.setOnMouseClicked(e -> {
 				TextField guessText = (TextField)game.lookup("#guessText");
-
 				if (guessText.getText().length() <= 0 || !onlyNumber(guessText.getText())) {
 					result.setText("invalid input!");
 					return;
 				}
 				yourGuess = Integer.valueOf(guessText.getText());
-				if (yourGuess > target) {
-					attempts--;
-					result.setText("too big! "+attempts+" left");
-				} else if (yourGuess < target) {
-					attempts--;
-					result.setText("too small! "+attempts+" left");
-				} else result.setText("bingo! the answer is: "+target);
-				
-				if (attempts <= 0) {
-					result.setText("you lose!");
+				if (yourGuess < min || yourGuess > max) {
+					result.setText("The range is "+min+" ~ "+max);
 					return;
 				}
+				if (yourGuess > target) {
+					attempts--;
+					result.setText("Too big, "+attempts+" times left");
+				} else if (yourGuess < target) {
+					attempts--;
+					result.setText("Too small, "+attempts+" times left");
+				} else {
+					result.setText("Bingo! the answer is: "+target);
+					initialize();
+				}
+				if (attempts <= 0) {
+					result.setText("you lose");
+					return;
+				}
+//				System.out.println(target);
 			});
 			Scene scene = new Scene(container, 600, 400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setResizable(false);
+			primaryStage.setTitle("Number Guessing Game");
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch (Exception e) {
